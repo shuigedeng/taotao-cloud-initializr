f5019179e5e7ee4920c565a30630cacc5d64c8e1
shuigedeng <981376577@qq.com>
2023-12-12 19:14:59 +0800

    build: 升级版本

--- start-site/src/main/java/io/spring/start/site/extension/dependency/taotaocloud/ModuleMavenBuild.java
+++ start-site/src/main/java/io/spring/start/site/extension/dependency/taotaocloud/ModuleMavenBuild.java
@@ -1,38 +1,38 @@
-package io.spring.start.site.extension.dependency.taotaocloud;
-
-import io.spring.initializr.generator.buildsystem.maven.MavenBuild;
-
-import java.util.List;
-
-public class ModuleMavenBuild extends MavenBuild {
-
-    private List<ModuleContainer> moduleContainer;
-
-    public List<ModuleContainer> getModuleContainer() {
-        return moduleContainer;
-    }
-
-    public ModuleMavenBuild setModuleContainer(List<ModuleContainer> moduleContainer) {
-        this.moduleContainer = moduleContainer;
-        return this;
-    }
-
-    public static class ModuleContainer {
-
-        private String moduleName;
-
-        public ModuleContainer(String moduleName) {
-            this.moduleName = moduleName;
-        }
-
-        public String getModuleName() {
-            return moduleName;
-        }
-
-        public ModuleContainer setModuleName(String moduleName) {
-            this.moduleName = moduleName;
-            return this;
-        }
-    }
-
-}
+//package io.spring.start.site.extension.dependency.taotaocloud;
+//
+//import io.spring.initializr.generator.buildsystem.maven.MavenBuild;
+//
+//import java.util.List;
+//
+//public class ModuleMavenBuild extends MavenBuild {
+//
+//    private List<ModuleContainer> moduleContainer;
+//
+//    public List<ModuleContainer> getModuleContainer() {
+//        return moduleContainer;
+//    }
+//
+//    public ModuleMavenBuild setModuleContainer(List<ModuleContainer> moduleContainer) {
+//        this.moduleContainer = moduleContainer;
+//        return this;
+//    }
+//
+//    public static class ModuleContainer {
+//
+//        private String moduleName;
+//
+//        public ModuleContainer(String moduleName) {
+//            this.moduleName = moduleName;
+//        }
+//
+//        public String getModuleName() {
+//            return moduleName;
+//        }
+//
+//        public ModuleContainer setModuleName(String moduleName) {
+//            this.moduleName = moduleName;
+//            return this;
+//        }
+//    }
+//
+//}
