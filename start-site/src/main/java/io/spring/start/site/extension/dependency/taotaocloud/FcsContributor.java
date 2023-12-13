0b6467084ba9424fbd7edc2772d7ffde5209b772
shuigedeng <981376577@qq.com>
2023-12-13 21:24:06 +0800

    build: 升级版本

---
+++ start-site/src/main/java/io/spring/start/site/extension/dependency/taotaocloud/FcsContributor.java
@@ -0,0 +1,49 @@
+package io.spring.start.site.extension.dependency.taotaocloud;
+
+import io.spring.initializr.generator.io.template.TemplateRenderer;
+import io.spring.initializr.generator.project.ProjectDescription;
+import io.spring.initializr.generator.project.contributor.ProjectContributor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FcsContributor implements ProjectContributor {
+    private static final Logger logger = LoggerFactory.getLogger(FcsContributor.class);
+
+    private final ProjectDescription description;
+    private final TemplateRenderer templateRenderer;
+    private final String SOURCE_PATH = "src/main/java/";
+
+    public FcsContributor(ProjectDescription description, TemplateRenderer templateRenderer) {
+        this.description = description;
+        this.templateRenderer = templateRenderer;
+    }
+
+    @Override
+    public void contribute(Path projectRoot) throws IOException {
+        Map<String, Object> data = new HashMap<>();
+        String groupId = description.getGroupId();
+        String artifactId = description.getArtifactId();
+        data.put("groupId", groupId);
+        data.put("artifactId", artifactId);
+
+        logger.info("data======: {}", data);
+
+        Path file = Files.createFile(projectRoot.resolve("README.md"));
+        String pre = SOURCE_PATH + groupId.replace(".", "/") + "/" + artifactId;
+        Files.createDirectories(projectRoot.resolve(pre + "/controller/"));
+        Path codeFile = Files.createFile(projectRoot.resolve(pre + "/controller/DemoController.java"));
+        String code = this.templateRenderer.render("code/DemoController.java", data);
+        try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(file));
+             PrintWriter codeWriter = new PrintWriter(Files.newBufferedWriter(codeFile));) {
+            writer.println("README");
+            codeWriter.print(code);
+        }
+    }
+}
--- start-site/src/main/java/io/spring/start/site/extension/dependency/taotaocloud/TaotaoCloudProjectGenerationConfiguration.java
+++ start-site/src/main/java/io/spring/start/site/extension/dependency/taotaocloud/TaotaoCloudProjectGenerationConfiguration.java
@@ -1,6 +1,8 @@
 package io.spring.start.site.extension.dependency.taotaocloud;
 
 import io.spring.initializr.generator.buildsystem.maven.MavenBuild;
+import io.spring.initializr.generator.condition.ConditionalOnBuildSystem;
+import io.spring.initializr.generator.condition.ConditionalOnRequestedDependency;
 import io.spring.initializr.generator.io.IndentingWriterFactory;
 import io.spring.initializr.generator.io.template.TemplateRenderer;
 import io.spring.initializr.generator.project.ProjectDescription;
@@ -11,6 +13,7 @@ import io.spring.initializr.metadata.InitializrMetadata;
 import org.springframework.context.annotation.Bean;
 
 @ProjectGenerationConfiguration
+@ConditionalOnRequestedDependency("taotao-cloud-starter-web")
 public class TaotaoCloudProjectGenerationConfiguration {
     private final InitializrMetadata initializrMetadata;
     private final ProjectDescription projectDescription;
@@ -26,37 +29,37 @@ public class TaotaoCloudProjectGenerationConfiguration {
         this.indentingWriterFactory = indentingWriterFactory;
         this.templateRenderer = templateRenderer;
     }
-    // @Bean
-    // @ConditionalOnRequestedDependency("sca-oss")
-    // public OSSDemoCodeContributor ossContributor() {
-    //     return new OSSDemoCodeContributor(description, templateRenderer);
-    // }
+
+    @Bean
+    public FcsContributor fcsContributor(ProjectDescription description,TemplateRenderer templateRenderer) {
+        return new FcsContributor(description, templateRenderer);
+    }
 
     // 添加README.md
-    @Bean
-    public ProjectContributor readmeProjectContributor() {
-        return new MultipleResourcesProjectContributor("contributor/readme");
-    }
+//    @Bean
+//    public ProjectContributor readmeProjectContributor() {
+//        return new MultipleResourcesProjectContributor("contributor/readme");
+//    }
 
-    /**
-     * 模块化的贡献者
-     *
-     * @return
-     */
-    @Bean
-    public ProjectContributor mouduleContributor() {
-        return new ModuleContributor(projectDescription, indentingWriterFactory, initializrMetadata);
-    }
-
-    /**
-     * 项目父结构下的pom.xml文件生成器 <br/>
-     * {@link MavenBuild} 没有module标签功能; 并且parent写死为spring-boot-dependencies
-     *
-     * @param build
-     * @return
-     */
-    @Bean
-    public CustomMavenBuildProjectContributor customMavenBuildProjectContributor(ModuleMavenBuild build) {
-        return new CustomMavenBuildProjectContributor(build, indentingWriterFactory);
-    }
+//    /**
+//     * 模块化的贡献者
+//     *
+//     * @return
+//     */
+//    @Bean
+//    public ProjectContributor mouduleContributor() {
+//        return new ModuleContributor(projectDescription, indentingWriterFactory, initializrMetadata);
+//    }
+//
+//    /**
+//     * 项目父结构下的pom.xml文件生成器 <br/>
+//     * {@link MavenBuild} 没有module标签功能; 并且parent写死为spring-boot-dependencies
+//     *
+//     * @param build
+//     * @return
+//     */
+//    @Bean
+//    public CustomMavenBuildProjectContributor customMavenBuildProjectContributor(ModuleMavenBuild build) {
+//        return new CustomMavenBuildProjectContributor(build, indentingWriterFactory);
+//    }
 }
